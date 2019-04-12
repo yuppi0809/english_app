bf6c43776b59c66c6c2540187c3ccee45da061cd
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-04-12 18:32:47 +0900

    define methods in application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  #ハイライトメソッドにテキストを渡す際、絶対検索した際のクオーテーションを消すようにする
+  def check_for_quotations(keyword)
+    keyword.match(/".+"/)
+  end
+
+  def remove_quotations(keyword)
+   return keyword.gsub(/^./, "").gsub(/.$/, "")
+  end
+
+  def display_search_word(keyword)
+    if check_for_quotations(keyword)
+      remove_quotations(keyword)
+    else
+      return keyword
+    end
+  end
+
+  def modify_text_for_highlight(keyword)
+    if check_for_quotations(keyword)
+      remove_quotations(keyword)
+    elsif keyword.match(//)
+      keyword.split(/\s/).reject {|word| word.length == 1 }
+    else
+      return keyword
+    end
+  end
 end
