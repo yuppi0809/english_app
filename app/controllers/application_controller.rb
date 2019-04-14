e3e76ee92b5c0f4a0d77a652080e7de97f5fd45c
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-04-14 23:57:54 +0900

    set basic_authentication for product enviornment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+
+  def production?
+    Rails.env.production?
+  end
 end
